b2481840503f103cb3a2a2ba0fa62f0a8b622203
justicepelteir <justice.douglas@hey.com>
2023-03-06 17:42:49 -0800

    add firebase url

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/tasks.json'
+        'https://react-http-8b7d6-default-rtdb.firebaseio.com/tasks.json'
       );
 
       if (!response.ok) {
